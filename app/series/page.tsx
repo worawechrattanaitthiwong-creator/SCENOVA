f69f174d4420fc8f8e1ceb47b618572a9d4d84e6
worawechrattanaitthiwong-creator <worawech.rattanaitthiwong@gmail.com>
2026-08-24 17:56:10 +0700

    feat: show deletable video history in Series

--- app/series/page.tsx
+++ app/series/page.tsx
@@ -1,5 +1,11 @@
 import SeriesManagerV3 from "@/components/series-manager-v3";
+import SeriesVideoHistory from "@/components/series-video-history";
 
 export default function SeriesPage() {
-  return <SeriesManagerV3 />;
+  return (
+    <>
+      <SeriesManagerV3 />
+      <SeriesVideoHistory />
+    </>
+  );
 }
