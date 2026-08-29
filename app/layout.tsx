edb8cc7430c06118fd0c2b0fcea6adfdeab3c63f
worawechrattanaitthiwong-creator <worawech.rattanaitthiwong@gmail.com>
2026-08-29 10:06:29 +0700

    Load unified API-style help system globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ import "./series-theme-fix-v5.css";
 import "./single-episode-compact-v1.css";
 import "./single-episode-options-v2.css";
 import "./settings-system-v1.css";
+import "./help-system-v2.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://scnva.com"),
